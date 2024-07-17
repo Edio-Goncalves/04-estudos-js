88f02ff692ed70549e7b50598b366657f9cb8f72
Edio Goncalves <ediogoncalvesjr@hotmail.com>
2024-07-17 17:32:50 -0300

    exercicio02 - filter()

--- exercicios/exercicio02.js
+++ exercicios/exercicio02.js
@@ -1,18 +1,30 @@
 const firstBox = document.querySelector("#firstBox");
 const secondBox = document.querySelector("#secondBox");
-
 const cursos = ["Javascript", "PHP", "React", "CSS", "JAVA", "C#"];
+const btnCursoSelecionado = document.querySelector(".cursoSelecionado");
 
-/* cria div dos cursos */
-cursos.map((e, i) => {
+/* adicionando div para cursos */
+
+cursos.map((e) => {
   const div = document.createElement("div");
-  div.setAttribute("id", "curso" + (i + 1));
+  div.setAttribute("id", "c1");
   div.setAttribute("class", "curso");
-  div.innerHTML = e;
   secondBox.appendChild(div);
+  div.innerHTML = e;
 
-  const seletor = document.createElement("input");
-  seletor.setAttribute("type", "radio");
-  seletor.setAttribute("name", "seletor");
-  div.appendChild(seletor);
+  const inputRadio = document.createElement("input");
+  inputRadio.setAttribute("type", "radio");
+  inputRadio.setAttribute("name", "InputRadio");
+  div.appendChild(inputRadio);
+});
+
+btnCursoSelecionado.addEventListener("click", () => {
+  const inputs = [...document.querySelectorAll("input[type=radio]")];
+  let inputFilter = inputs.filter((e) => {
+    return e.checked;
+  });
+  inputFilter = inputFilter[0];
+  const textInput = inputFilter.parentNode.firstChild.textContent;
+
+  alert(textInput);
 });
